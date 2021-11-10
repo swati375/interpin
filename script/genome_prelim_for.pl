b22f978309a072ec802d703acd621e4790ce30c6
Swati Gupta <77925678+swati375@users.noreply.github.com>
2021-11-10 18:16:08 +0530

    Delete script directory

--- script/genome_prelim_for.pl
+++
@@ -1,9 +0,0 @@
-#!usr/local/bin/perl5 -w
-no warnings 'uninitialized';
-use Cwd;
-require 'run_mfold_for.pl';
-require 'prep_inputhp_for.pl';
-
-#program to run mfold on sequences and extract out hp from mfold output for each seq
-&mfold_for($ARGV[0]);
-&prep_inputhairpin_for($ARGV[0]);
--- script/genome_prelim_rev.pl
+++
@@ -1,8 +0,0 @@
-#!usr/local/bin/perl5 -w
-no warnings 'uninitialized';
-use Cwd;
-require 'run_mfold_rev.pl';
-require 'prep_inputhp_rev.pl';
-
-&mfold_rev($ARGV[0]);
-&prep_inputhairpin_rev($ARGV[0]);
